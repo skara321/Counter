556b0a12971f03586aad29594b7622e58e74d0cd
skara321 <skara654321@gmail.com>
2024-09-16 21:11:59 +0300

    first commit

---
+++ scripts/sendIncrement.ts
@@ -0,0 +1,44 @@
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { mnemonicToWalletKey } from "ton-crypto";
+import { TonClient, WalletContractV4, Address } from "@ton/ton";
+import Counter from "../wrappers/Counter"; // this is the interface class we just implemented
+
+export async function run() {
+  // initialize ton rpc client on testnet
+  const endpoint = await getHttpEndpoint({ network: "testnet" });
+  const client = new TonClient({ endpoint });
+
+  // open wallet v4 (notice the correct wallet version here)
+  const mnemonic = "trophy suggest roast depth crystal across all blind trade toward case atom dumb learn cliff clap affair wear jump acid bounce entire spin avoid"; // your 24 secret words (replace ... with the rest of the words)
+  const key = await mnemonicToWalletKey(mnemonic.split(" "));
+  const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+  if (!await client.isContractDeployed(wallet.address)) {
+    return console.log("wallet is not deployed");
+  }
+
+  // open wallet and read the current seqno of the wallet
+  const walletContract = client.open(wallet);
+  const walletSender = walletContract.sender(key.secretKey);
+  const seqno = await walletContract.getSeqno();
+
+  // open Counter instance by address
+  const counterAddress = Address.parse("UQAybz-vXBds6v9BaxwVdD1xHltYDEO9zdtk-RN81ekwu-dE"); // replace with your address from step 8
+  const counter = new Counter(counterAddress);
+  const counterContract = client.open(counter);
+
+  // send the increment transaction
+  await counterContract.sendIncrement(walletSender);
+
+  // wait until confirmed
+  let currentSeqno = seqno;
+  while (currentSeqno == seqno) {
+    console.log("waiting for transaction to confirm...");
+    await sleep(1500);
+    currentSeqno = await walletContract.getSeqno();
+  }
+  console.log("transaction confirmed!");
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
